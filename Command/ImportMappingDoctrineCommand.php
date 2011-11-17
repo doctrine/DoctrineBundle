f92d1778a2b70058521c9562753905be6b721129
Benjamin Eberlei <kontakt@beberlei.de>
2011-11-17 22:55:39 +0100

    Fix LICENSE

--- Command/ImportMappingDoctrineCommand.php
+++ Command/ImportMappingDoctrineCommand.php
@@ -1,9 +1,12 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the Doctrine Bundle
+ *
+ * The code was originally distributed inside the Symfony framework.
  *
  * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- Command/Proxy/DoctrineCommandHelper.php
+++ Command/Proxy/DoctrineCommandHelper.php
@@ -1,14 +1,16 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the Doctrine Bundle
+ *
+ * The code was originally distributed inside the Symfony framework.
  *
  * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Doctrine\Bundle\DoctrineBundle\Command\Proxy;
 
 use Symfony\Component\Console\Application;
