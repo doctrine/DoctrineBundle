9c8fb385d953ef1249ff540c7e0ec59a41f1a02b
Christophe Coevoet <stof@notk.org>
2011-12-19 22:06:49 +0100

    [DoctrineBridge] Made it possible to change the manager used by the provider

--- DependencyInjection/Security/UserProvider/EntityFactory.php
+++ DependencyInjection/Security/UserProvider/EntityFactory.php
@@ -34,6 +34,7 @@ class EntityFactory implements UserProviderFactoryInterface
             ->setDefinition($id, new DefinitionDecorator('doctrine.orm.security.user.provider'))
             ->addArgument($config['class'])
             ->addArgument($config['property'])
+            ->addArgument($config['manager_name'])
         ;
     }
 
@@ -48,6 +49,7 @@ class EntityFactory implements UserProviderFactoryInterface
             ->children()
                 ->scalarNode('class')->isRequired()->cannotBeEmpty()->end()
                 ->scalarNode('property')->defaultNull()->end()
+                ->scalarNode('manager_name')->defaultNull()->end()
             ->end()
         ;
     }
