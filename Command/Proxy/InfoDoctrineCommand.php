65a9322f8a2a5e8cb8af4f20fb069bb9fc62192b
Christophe Coevoet <stof@notk.org>
2012-04-01 21:49:48 +0200

    Removed the InfoCommand and proxy the ORM command instead
    The command was added in the ORM 2.1 so there is no need to keep it in
    the bundle.

---
+++ Command/Proxy/InfoDoctrineCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Doctrine Bundle
+ *
+ * The code was originally distributed inside the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Doctrine\Bundle\DoctrineBundle\Command\Proxy;
+
+use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\ORM\Tools\Console\Command\InfoCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Show information about mapped entities
+ *
+ * @author Benjamin Eberlei <kontakt@beberlei.de>
+ */
+class InfoDoctrineCommand extends InfoCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('doctrine:mapping:info')
+            ->addOption('em', null, InputOption::VALUE_OPTIONAL, 'The entity manager to use for this command')
+            ->setDescription('Shows basic information about all mapped entities')
+            ->setHelp(<<<EOT
+The <info>doctrine:mapping:info</info> shows basic information about which
+entities exist and possibly if their mapping information contains errors or
+not.
+
+<info>php app/console doctrine:mapping:info</info>
+
+If you are using multiple entity managers you can pick your choice with the
+<info>--em</info> option:
+
+<info>php app/console doctrine:mapping:info --em=default</info>
+EOT
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        DoctrineCommandHelper::setApplicationEntityManager($this->getApplication(), $input->getOption('em'));
+
+        parent::execute($input, $output);
+    }
+}
