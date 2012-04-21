8106a2214478eaf5f780362c413ae135c77de4b2
Bilal Amarni <bilal.amarni@gmail.com>
2012-04-21 12:34:47 +0200

    added a way to use cache services

--- DependencyInjection/DoctrineExtension.php
+++ DependencyInjection/DoctrineExtension.php
@@ -411,8 +411,14 @@ class DoctrineExtension extends AbstractDoctrineExtension
         $cacheDriverService = sprintf('doctrine.orm.%s_%s', $entityManager['name'], $cacheName);
 
         $driver = $cacheName."_driver";
-        $cacheDef = $this->getEntityManagerCacheDefinition($entityManager, $entityManager[$driver], $container);
-        $container->setDefinition($cacheDriverService, $cacheDef);
+        $cacheDriver = $entityManager[$driver];
+
+        if ('service' === $cacheDriver['type']) {
+            $container->setAlias($cacheDriverService, $cacheDriver['id']);
+        } else {
+            $cacheDef = $this->getEntityManagerCacheDefinition($entityManager, $cacheDriver, $container);
+            $container->setDefinition($cacheDriverService, $cacheDef);
+        }
     }
 
     /**
