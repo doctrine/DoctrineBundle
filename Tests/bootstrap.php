4117f585f003c4bc373503b8351bec3866781f95
Bilal Amarni <bilal.amarni@gmail.com>
2012-06-19 13:34:51 +0300

    typo in test bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
+if (!@include __DIR__ . '/../vendor/autoload.php') {
     die("You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
-php composer.phar install
+php composer.phar install --dev
 ");
 }
