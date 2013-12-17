671edc6d95facc7c9d64a9e3ad13ecf7a47f6514
Ilya Biryukov <ilya@goparty.ie>
2013-12-17 02:40:37 +0000

    Bug fix for SQL parameter replacing function. It will not handle Postgres casting operations correctly (::datatype) and won't try to replace them with parameters

---
+++ Tests/Twig/DoctrineExtensionTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the Doctrine Bundle
+ *
+ * The code was originally distributed inside the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+namespace Doctrine\Bundle\DoctrineBundle\Tests\Twig;
+
+use Doctrine\Bundle\DoctrineBundle\Twig\DoctrineExtension;
+
+class DoctrineExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReplaceQueryParametersWithPostgresCasting()
+    {
+        $extension = new DoctrineExtension();
+        $query = "a=? OR (1)::string OR b=?";
+        $parameters = array(1, 2);
+
+        $result = $extension->replaceQueryParameters($query, $parameters, false);
+        $this->assertEquals("a=1 OR (1)::string OR b=2", $result);
+    }
+}
