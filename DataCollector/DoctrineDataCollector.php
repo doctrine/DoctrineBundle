3bfc872b2d263368763616a4d592f99007659369
mike <mike.simonson@gmail.com>
2017-03-07 14:34:01 +0100

    Fix division by zero in the data collector

--- DataCollector/DoctrineDataCollector.php
+++ DataCollector/DoctrineDataCollector.php
@@ -222,16 +222,24 @@ class DoctrineDataCollector extends BaseCollector
             });
             $groupedQueries[$connection] = $connectionGroupedQueries;
         }
-        if ((float) $totalExecutionMS === 0.0) {
-            $totalExecutionMS = 1;
-        }
+
         foreach ($groupedQueries as $connection => $queries) {
             foreach ($queries as $i => $query) {
-                $groupedQueries[$connection][$i]['executionPercent'] = $query['executionMS'] / $totalExecutionMS * 100;
+                $groupedQueries[$connection][$i]['executionPercent'] =
+                    $this->executionTimePercentage($query['executionMS'], $totalExecutionMS );
             }
         }
 
         return $groupedQueries;
+    }
+
+    private function executionTimePercentage($executionTimeMS, $totalExecutionTimeMS)
+    {
+        if ($totalExecutionTimeMS === 0.0 || $totalExecutionTimeMS === 0) {
+            return 0;
+        }
+
+        return $executionTimeMS / $totalExecutionTimeMS * 100;
     }
 
     public function getGroupedQueryCount()
