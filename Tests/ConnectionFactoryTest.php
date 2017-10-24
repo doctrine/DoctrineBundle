a8995708341e038bc020569a41a1bdd7c3a81c4c
Ryan Weaver <ryan@thatsquality.com>
2017-10-24 11:04:16 -0400

    Fixing tests
    While trying to mock the DriverInterface directly, PhpUnit throws:
    
    > Class cannot implement interface Throwable
    
    from the mocked class

--- Tests/ConnectionFactoryTest.php
+++ Tests/ConnectionFactoryTest.php
@@ -41,7 +41,7 @@ class ConnectionFactoryTest extends TestCase
         $config       = null;
         $eventManager = null;
         $mappingTypes = [0];
-        $exception    = new DriverException('', $this->createMock('\Doctrine\DBAL\Driver\DriverException'));
+        $exception    = new DriverException('', $this->createMock(Driver\AbstractDriverException::class));
 
         // put the mock into the fake driver
         FakeDriver::$exception = $exception;
