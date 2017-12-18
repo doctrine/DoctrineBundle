4ab04c00f1c6449c4aaa3fc60d57c3238ea3530e
mikeSimonson <mike.simonson@gmail.com>
2017-12-18 09:36:12 +0100

    Removing more headers (#751)

--- Command/Proxy/DoctrineCommandHelper.php
+++ Command/Proxy/DoctrineCommandHelper.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * This file is part of the Doctrine Bundle
- *
- * The code was originally distributed inside the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 namespace Doctrine\Bundle\DoctrineBundle\Command\Proxy;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
