23a025f049b7e764292779fcdbc1c94bc871cfc7
Eric Shishko <eriks.sisko@gmail.com>
2018-02-14 10:51:37 +0200

    Fix getting service repository from factory with different em

--- Repository/ServiceEntityRepositoryInterface.php
+++ Repository/ServiceEntityRepositoryInterface.php
@@ -2,9 +2,17 @@
 
 namespace Doctrine\Bundle\DoctrineBundle\Repository;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 /**
  * This interface signals that your repository should be loaded from the container.
  */
 interface ServiceEntityRepositoryInterface
 {
+    /**
+     * @param EntityManagerInterface $em
+     *
+     * @return mixed
+     */
+    public function setEntityManager(EntityManagerInterface $em);
 }
