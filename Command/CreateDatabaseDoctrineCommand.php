967c7a5fe926142b4b458e0f422f8d19bd98cf16
Oskar Stark <oskarstark@googlemail.com>
2018-06-22 10:47:18 +0200

    fixed comment

--- Command/CreateDatabaseDoctrineCommand.php
+++ Command/CreateDatabaseDoctrineCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Database tool allows you to easily drop and create your configured databases.
+ * Database tool allows you to easily create your configured databases.
  */
 class CreateDatabaseDoctrineCommand extends DoctrineCommand
 {
