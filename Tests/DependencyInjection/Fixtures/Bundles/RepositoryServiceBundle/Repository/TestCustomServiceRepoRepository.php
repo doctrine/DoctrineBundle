43a368509ee8ff2028e042032d3bbc4f10712028
Andreas Braun <git@alcaeus.org>
2019-11-11 20:12:18 +0100

    Add new service alias for manager registry

--- Tests/DependencyInjection/Fixtures/Bundles/RepositoryServiceBundle/Repository/TestCustomServiceRepoRepository.php
+++ Tests/DependencyInjection/Fixtures/Bundles/RepositoryServiceBundle/Repository/TestCustomServiceRepoRepository.php
@@ -2,13 +2,13 @@
 
 namespace Fixtures\Bundles\RepositoryServiceBundle\Repository;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Fixtures\Bundles\RepositoryServiceBundle\Entity\TestCustomServiceRepoEntity;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class TestCustomServiceRepoRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(Registry $registry)
     {
         parent::__construct($registry, TestCustomServiceRepoEntity::class);
     }
