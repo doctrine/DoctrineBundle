84321a11a9004e3edc276505ca9a816090d4a0e1
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2019-12-01 15:17:34 +0100

    Deprecate doctrine:mapping:import

--- Command/ImportMappingDoctrineCommand.php
+++ Command/ImportMappingDoctrineCommand.php
@@ -16,6 +16,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  * Import Doctrine ORM metadata mapping information from an existing database.
  *
+ * @deprecated
+ *
  * @final
  */
 class ImportMappingDoctrineCommand extends DoctrineCommand
---
+++ UPGRADE-2.1.md
@@ -0,0 +1,7 @@
+UPGRADE FROM 2.0 to 2.1
+=======================
+
+Commands
+--------
+
+ * `Doctrine\Bundle\DoctrineBundle\Command\ImportMappingDoctrineCommand` has been deprecated  
