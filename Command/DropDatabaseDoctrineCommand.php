0bd9f7fa1d3556f2590051608d0382b232178721
Pete Walker <pete.walker@ourpeople.com>
2020-11-10 18:01:08 +0000

    fix: Lint / test issues

--- Command/DropDatabaseDoctrineCommand.php
+++ Command/DropDatabaseDoctrineCommand.php
@@ -62,20 +62,20 @@ EOT
         $ifExists = $input->getOption('if-exists');
         
         $driverOptions = [];
-        $params = $connection->getParams();
-        
+        $params        = $connection->getParams();
+
         if (isset($params['driverOptions'])) {
-            $driverOptions = $params['driverOptions']
+            $driverOptions = $params['driverOptions'];
         }
-        
+
         if (isset($params['master'])) {
-            $params = $params['master'];
+            $params                  = $params['master'];
             $params['driverOptions'] = $driverOptions;
         }
 
         // Since doctrine/dbal 2.11 master has been replaced by primary
         if (isset($params['primary'])) {
-            $params = $params['primary'];
+            $params                  = $params['primary'];
             $params['driverOptions'] = $driverOptions;
         }
 
