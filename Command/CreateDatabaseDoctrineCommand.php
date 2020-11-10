52c046f4255179dac677c9082cebd9dbb5a4f27d
Pete Walker <pete.walker@ourpeople.com>
2020-11-10 17:46:51 +0000

    fix: Revert #1240 and allow master connections to respect driverOptions
    Reverts the changes made in #1240, and applies the passing down of `driverOptions` manually in `CreateDatabaseDoctrineCommand` and `DropDatabaseDoctrineCommand`
    
    Resolves #1252

--- Command/CreateDatabaseDoctrineCommand.php
+++ Command/CreateDatabaseDoctrineCommand.php
@@ -52,14 +52,22 @@ EOT
 
         $ifNotExists = $input->getOption('if-not-exists');
 
+        $driverOptions = [];
         $params = $connection->getParams();
+
+        if (isset($params['driverOptions'])) {
+            $driverOptions = $params['driverOptions']
+        }
+
         if (isset($params['master'])) {
             $params = $params['master'];
+            $params['driverOptions'] = $driverOptions;
         }
 
         // Since doctrine/dbal 2.11 master has been replaced by primary
         if (isset($params['primary'])) {
             $params = $params['primary'];
+            $params['driverOptions'] = $driverOptions;
         }
 
         // Cannot inject `shard` option in parent::getDoctrineConnection
