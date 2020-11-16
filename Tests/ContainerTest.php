2c377e26e8f54590dbacd0e8d49c10666c354860
Grégoire Paris <postmaster@greg0ire.fr>
2020-11-16 09:22:08 +0100

    Pass loggers through the constructor
    Passing them through addLogger() is deprecated and no longer works with
    DBAL 3 which we support.

--- Tests/ContainerTest.php
+++ Tests/ContainerTest.php
@@ -2,10 +2,12 @@
 
 namespace Doctrine\Bundle\DoctrineBundle\Tests;
 
+use Doctrine\Bundle\DoctrineBundle\Tests\DependencyInjection\Fixtures\DbalTestKernel;
 use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Configuration as DBALConfiguration;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Logging\LoggerChain;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
@@ -22,13 +24,16 @@ use Symfony\Component\Cache\DoctrineProvider;
 
 class ContainerTest extends TestCase
 {
-    public static function setUpBeforeClass(): void
+    public function testContainerWithDbalOnly(): void
     {
-        if (interface_exists(EntityManagerInterface::class)) {
-            return;
-        }
+        $kernel = new DbalTestKernel();
+        $kernel->boot();
 
-        self::markTestSkipped('This test requires ORM');
+        $container = $kernel->getContainer();
+        $this->assertInstanceOf(
+            LoggerChain::class,
+            $container->get('doctrine.dbal.logger.chain.default')
+        );
     }
 
     /**
@@ -38,6 +43,10 @@ class ContainerTest extends TestCase
      */
     public function testContainer(): void
     {
+        if (! interface_exists(EntityManagerInterface::class)) {
+            self::markTestSkipped('This test requires ORM');
+        }
+
         $container = $this->createXmlBundleTestContainer();
 
         $this->assertInstanceOf(DbalLogger::class, $container->get('doctrine.dbal.logger'));
