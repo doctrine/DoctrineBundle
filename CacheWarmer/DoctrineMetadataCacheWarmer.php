doctrine/DoctrineBundle

commit 155a870ecbd035963e6b450e0f235e69c48cab30

Remove call to deprecated setCacheDriver() method

CacheWarmer/DoctrineMetadataCacheWarmer.php
@@ -6,7 +6,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\CacheWarmer\AbstractPhpFileCacheWarmer;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
-use Symfony\Component\Cache\DoctrineProvider;
+
+use function method_exists;
 
 class DoctrineMetadataCacheWarmer extends AbstractPhpFileCacheWarmer
 {
@@ -47,7 +48,13 @@ class DoctrineMetadataCacheWarmer extends AbstractPhpFileCacheWarmer
             throw new LogicException('DoctrineMetadataCacheWarmer must load metadata first, check priority of your warmers.');
         }
 
-        $metadataFactory->setCacheDriver(new DoctrineProvider($arrayAdapter));
+        if (method_exists($metadataFactory, 'setCache')) {
+            $metadataFactory->setCache($arrayAdapter);
+        } else {
+            // BC with doctrine/persistence < 2.2
+            $metadataFactory->setCacheDriver(new DoctrineProvider($arrayAdapter));
+        }
+
         $metadataFactory->getAllMetadata();
 
         return true;

commit 8f79f2655b23b1a25df74886e745dc6cf63c44eb

Fix broken persistence < 2.2 BC in DoctrineMetadataCacheWarmer
Was regression from 155a870ecbd035963e6b450e0f235e69c48cab30

<3 Psalm

CacheWarmer/DoctrineMetadataCacheWarmer.php
@@ -6,8 +6,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\CacheWarmer\AbstractPhpFileCacheWarmer;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\DoctrineProvider;
 
-use function count;
 use function is_file;
 use function method_exists;
 
@@ -52,7 +52,7 @@ class DoctrineMetadataCacheWarmer extends AbstractPhpFileCacheWarmer
 
         if (method_exists($metadataFactory, 'setCache')) {
             $metadataFactory->setCache($arrayAdapter);
-        } else {
+        } elseif ($metadataFactory instanceof AbstractClassMetadataFactory) {
             // BC with doctrine/persistence < 2.2
             $metadataFactory->setCacheDriver(new DoctrineProvider($arrayAdapter));
         }