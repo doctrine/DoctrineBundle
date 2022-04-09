d241408402439a95d3f9e838a74754d3d55f2407
Simon Podlipsky <simon@podlipsky.net>
2022-04-09 14:34:09 +0200

    fix: propagate driverOptions to primary-replica connections

--- DependencyInjection/DoctrineExtension.php
+++ DependencyInjection/DoctrineExtension.php
@@ -360,10 +360,21 @@ class DoctrineExtension extends AbstractDoctrineExtension
             throw new InvalidArgumentException('Sharding and master-slave connection cannot be used together');
         }
 
+        foreach (['shards', 'replica', 'slaves'] as $connectionKey) {
+            foreach (array_keys($options[$connectionKey]) as $name) {
+                $driverOptions       = $options[$connectionKey][$name]['driverOptions'] ?? [];
+                $parentDriverOptions = $options['driverOptions'] ?? [];
+                if ($driverOptions === [] && $parentDriverOptions === []) {
+                    continue;
+                }
+
+                $options[$connectionKey][$name]['driverOptions'] = $driverOptions + $parentDriverOptions;
+            }
+        }
+
         if (! empty($options['slaves']) || ! empty($options['replica'])) {
             $nonRewrittenKeys = [
                 'driver' => true,
-                'driverOptions' => true,
                 'driverClass' => true,
                 'wrapperClass' => true,
                 'keepSlave' => true,
