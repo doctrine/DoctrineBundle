5546ade36f66db981f3d2a698a6eaa3b536f58c0
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-12-27 12:41:16 +0100

    Make ServiceEntityRepository lazy on Symfony 6.2+

--- Repository/ServiceEntityRepository.php
+++ Repository/ServiceEntityRepository.php
@@ -5,43 +5,51 @@ namespace Doctrine\Bundle\DoctrineBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use LogicException;
+use Symfony\Component\VarExporter\LazyGhostTrait;
 
 use function sprintf;
+use function trait_exists;
 
-/**
- * Optional EntityRepository base class with a simplified constructor (for autowiring).
- *
- * To use in your class, inject the "registry" service and call
- * the parent constructor. For example:
- *
- * class YourEntityRepository extends ServiceEntityRepository
- * {
- *     public function __construct(ManagerRegistry $registry)
- *     {
- *         parent::__construct($registry, YourEntity::class);
- *     }
- * }
- *
- * @template T of object
- * @template-extends EntityRepository<T>
- */
-class ServiceEntityRepository extends EntityRepository implements ServiceEntityRepositoryInterface
-{
-    /**
-     * @param string $entityClass The class name of the entity this repository manages
-     * @psalm-param class-string<T> $entityClass
-     */
-    public function __construct(ManagerRegistry $registry, string $entityClass)
+if (trait_exists(LazyGhostTrait::class)) {
+    class ServiceEntityRepository extends LazyServiceEntityRepository
     {
-        $manager = $registry->getManagerForClass($entityClass);
+    }
+} else {
+    /**
+     * Optional EntityRepository base class with a simplified constructor (for autowiring).
+     *
+     * To use in your class, inject the "registry" service and call
+     * the parent constructor. For example:
+     *
+     * class YourEntityRepository extends ServiceEntityRepository
+     * {
+     *     public function __construct(ManagerRegistry $registry)
+     *     {
+     *         parent::__construct($registry, YourEntity::class);
+     *     }
+     * }
+     *
+     * @template T of object
+     * @template-extends EntityRepository<T>
+     */
+    class ServiceEntityRepository extends EntityRepository implements ServiceEntityRepositoryInterface
+    {
+        /**
+         * @param string $entityClass The class name of the entity this repository manages
+         * @psalm-param class-string<T> $entityClass
+         */
+        public function __construct(ManagerRegistry $registry, string $entityClass)
+        {
+            $manager = $registry->getManagerForClass($entityClass);
 
-        if ($manager === null) {
-            throw new LogicException(sprintf(
-                'Could not find the entity manager for class "%s". Check your Doctrine configuration to make sure it is configured to load this entity’s metadata.',
-                $entityClass
-            ));
+            if ($manager === null) {
+                throw new LogicException(sprintf(
+                    'Could not find the entity manager for class "%s". Check your Doctrine configuration to make sure it is configured to load this entity’s metadata.',
+                    $entityClass
+                ));
+            }
+
+            parent::__construct($manager, $manager->getClassMetadata($entityClass));
         }
-
-        parent::__construct($manager, $manager->getClassMetadata($entityClass));
     }
 }
