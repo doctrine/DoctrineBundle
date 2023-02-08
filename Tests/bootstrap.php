af864c9079d79f966b2890c1ce91a61ea59df001
David Maicher <mail@dmaicher.de>
2023-02-08 11:23:11 +0100

    require doctrine/deprecations

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,6 +4,4 @@ use Doctrine\Deprecations\Deprecation;
 
 require_once 'vendor/autoload.php';
 
-if (class_exists(Deprecation::class)) {
-    Deprecation::enableWithTriggerError();
-}
+Deprecation::enableWithTriggerError();
