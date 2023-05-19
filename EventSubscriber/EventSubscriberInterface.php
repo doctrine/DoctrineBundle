656beec9a163b44987a7e79f89f74020d0358b17
Allison Guilhem <allison.guilhem@gmail.com>
2023-05-19 11:09:08 +0200

    [DoctrineBundle] deprecate EventSubscriberInterface

--- EventSubscriber/EventSubscriberInterface.php
+++ EventSubscriber/EventSubscriberInterface.php
@@ -4,6 +4,9 @@ namespace Doctrine\Bundle\DoctrineBundle\EventSubscriber;
 
 use Doctrine\Common\EventSubscriber;
 
+/**
+ * @deprecated use the {@see AsDoctrineListener} attribute instead
+ */
 interface EventSubscriberInterface extends EventSubscriber
 {
 }
---
+++ UPGRADE-2.10.md
@@ -0,0 +1,6 @@
+UPGRADE FROM 2.9 to 2.10
+========================
+
+### Deprecations
+
+- `Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface` has been deprecated. Use the `#[AsDoctrineListener]` attribute instead.
