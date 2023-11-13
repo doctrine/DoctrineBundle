f7f2146e3baa23fd5fd734d724b20f41c0fa298b
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-11-13 19:10:38 +0100

    Don't rely on LazyProxyTrait in LazyServiceEntityRepository

--- Repository/ServiceEntityRepository.php
+++ Repository/ServiceEntityRepository.php
@@ -3,56 +3,30 @@
 namespace Doctrine\Bundle\DoctrineBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Symfony\Component\VarExporter\LazyGhostTrait;
 
 use function property_exists;
-use function trait_exists;
 
 if (property_exists(EntityRepository::class, '_entityName')) {
-    if (trait_exists(LazyGhostTrait::class)) {
-        // ORM 2 with VarExporter
-        /**
-         * Optional EntityRepository base class with a simplified constructor (for autowiring).
-         *
-         * To use in your class, inject the "registry" service and call
-         * the parent constructor. For example:
-         *
-         * class YourEntityRepository extends ServiceEntityRepository
-         * {
-         *     public function __construct(ManagerRegistry $registry)
-         *     {
-         *         parent::__construct($registry, YourEntity::class);
-         *     }
-         * }
-         *
-         * @template T of object
-         * @template-extends LazyServiceEntityRepository<T>
-         */
-        class ServiceEntityRepository extends LazyServiceEntityRepository
-        {
-        }
-    } else {
-        // ORM 2 without VarExporter
-        /**
-         * Optional EntityRepository base class with a simplified constructor (for autowiring).
-         *
-         * To use in your class, inject the "registry" service and call
-         * the parent constructor. For example:
-         *
-         * class YourEntityRepository extends ServiceEntityRepository
-         * {
-         *     public function __construct(ManagerRegistry $registry)
-         *     {
-         *         parent::__construct($registry, YourEntity::class);
-         *     }
-         * }
-         *
-         * @template T of object
-         * @template-extends LegacyServiceEntityRepository<T>
-         */
-        class ServiceEntityRepository extends LegacyServiceEntityRepository
-        {
-        }
+    // ORM 2
+    /**
+     * Optional EntityRepository base class with a simplified constructor (for autowiring).
+     *
+     * To use in your class, inject the "registry" service and call
+     * the parent constructor. For example:
+     *
+     * class YourEntityRepository extends ServiceEntityRepository
+     * {
+     *     public function __construct(ManagerRegistry $registry)
+     *     {
+     *         parent::__construct($registry, YourEntity::class);
+     *     }
+     * }
+     *
+     * @template T of object
+     * @template-extends LazyServiceEntityRepository<T>
+     */
+    class ServiceEntityRepository extends LazyServiceEntityRepository
+    {
     }
 } else {
     // ORM 3
