0ad642e19a0169b1a14d05b91ccb03873e028f4f
Mati Kochen <mati@awareways.com>
2025-01-30 09:25:26 +0100

    feat: allow doctrine/persistence v4

--- src/Mapping/MappingDriver.php
+++ src/Mapping/MappingDriver.php
@@ -21,7 +21,7 @@ class MappingDriver implements MappingDriverInterface
     /**
      * {@inheritDoc}
      */
-    public function getAllClassNames()
+    public function getAllClassNames(): array
     {
         return $this->driver->getAllClassNames();
     }
