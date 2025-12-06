162ad34a19ce0a3c171ea8885c35e832d594fff1
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2025-12-06 09:44:24 +0100

    fix: addd phpdoc in method argument

--- tests/DataCollector/DoctrineDataCollectorTest.php
+++ tests/DataCollector/DoctrineDataCollectorTest.php
@@ -51,8 +51,11 @@ class DoctrineDataCollectorTest extends TestCase
             ->willReturn(false);
 
         $metadatas = [
+            /** @phpstan-ignore argument.type */
             $this->createEntityMetadata(self::FIRST_ENTITY),
+            /** @phpstan-ignore argument.type */
             $this->createEntityMetadata(self::SECOND_ENTITY),
+            /** @phpstan-ignore argument.type */
             $this->createEntityMetadata(self::FIRST_ENTITY),
         ];
         $factory->expects($this->once())
@@ -136,11 +139,13 @@ class DoctrineDataCollectorTest extends TestCase
         $this->assertSame(1, $groupedQueries['default'][1]['count']);
     }
 
-    /** @return ClassMetadata<object> */
+    /**
+     * @param class-string $entityFQCN
+     *
+     * @return ClassMetadata<object>
+     */
     private function createEntityMetadata(string $entityFQCN): ClassMetadata
     {
-        /** @var class-string $entityFQCN */
-        $entityFQCN          = $entityFQCN;
         $metadata            = new ClassMetadata($entityFQCN);
         $metadata->name      = $entityFQCN;
         $metadata->reflClass = new ReflectionClass('stdClass');
